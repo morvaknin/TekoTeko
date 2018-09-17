a7961512aab3f5ed4b8171192275055c76afdead
Mor Vaknin <morhai91@gmail.com>
2018-09-17 18:16:52 +0300

    chooseWinner works

--- Project/javaScript/index.js
+++ Project/javaScript/index.js
@@ -25,3 +25,48 @@ window.onload = function(){
 
     
 }
+
+var title = document.getElementById("title1");
+var eight_final = document.getElementById("tree").getElementsByClassName("8Final");
+var q_final = document.getElementById("tree").getElementsByClassName("QFinal");
+var s_final = document.getElementById("tree").getElementsByClassName("semiFinal");
+var _final = document.getElementById("tree").getElementsByClassName("Final");
+var _winner = document.getElementById("tree").getElementsByClassName("Winner");
+const _teams = ["Uruguay","Portugal","France","Argentina","Brazil","Mexico","Belgium",
+"Japan","Spain","Russia","Croatia","Denmark","Sweden","Swiss","Colombia","England"];
+
+console.log(q_final);
+
+
+//init Round of 16
+for (i=0; i<eight_final.length;i++){
+    eight_final[i].innerHTML = _teams[i];
+}
+
+
+for (i=0; i<eight_final.length;i++){
+    const index = i;
+    eight_final[i].addEventListener("click",function(){chooseWinner(eight_final,q_final,index);});
+}
+
+for (i=0; i<q_final.length;i++){
+    const index = i;
+    q_final[i].addEventListener("click",function(){chooseWinner(q_final,s_final,index);});
+}
+
+for (i=0; i<s_final.length;i++){
+    const index = i;
+    s_final[i].addEventListener("click",function(){chooseWinner(s_final,_final,index);});
+}
+
+for (i=0; i<_final.length;i++){
+    const index = i;
+    _final[i].addEventListener("click",function(){chooseWinner(_final,_winner,index);});
+}
+
+
+
+function chooseWinner(prevStage,currentStage,index){
+    var j = Math.floor(index/2);
+    currentStage[j].innerHTML=prevStage[index].innerHTML;
+}
