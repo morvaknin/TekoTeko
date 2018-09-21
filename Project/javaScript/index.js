f637748e40acf5b347ce70d603654f2ba8ca2809
Mor Vaknin <morhai91@gmail.com>
2018-09-22 00:57:02 +0300

    all works

--- Project/javaScript/index.js
+++ Project/javaScript/index.js
@@ -1,39 +1,44 @@
-window.onload = function(){
-
-    var modal = document.getElementById('myModal');
-    var span = document.getElementsByClassName("close")[0];
-    var guessBtn = document.getElementById("guessBTN");
-
-
-
-    // When the user clicks on the button, open the modal 
-    guessBtn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-
-    
-}
-
 var eight_final = document.getElementById("tree").getElementsByClassName("8Final");
 var q_final = document.getElementById("tree").getElementsByClassName("QFinal");
 var s_final = document.getElementById("tree").getElementsByClassName("semiFinal");
 var _final = document.getElementById("tree").getElementsByClassName("Final");
 var _winner = document.getElementById("tree").getElementsByClassName("Winner");
+var _message = document.getElementById("guess_sent_pop_up");
+
 const _teams = ["Uruguay","Portugal","France","Argentina","Brazil","Mexico","Belgium",
 "Japan","Spain","Russia","Croatia","Denmark","Sweden","Swiss","Colombia","England"];
 
+
+
+
+function request(method, url, _data) {
+    let data = _data || {};
+    return new Promise((resolve, reject) => {
+        let httpClient = new XMLHttpRequest();
+
+        // configure request type
+        httpClient.open(method, url, true);
+        httpClient.setRequestHeader("Content-type", "application/json");
+
+        // handle response
+        httpClient.onreadystatechange = () => {
+            if (httpClient.readyState == 4) {
+                var response = httpClient.response;
+                if(httpClient.status == 200) {
+                    // success
+                    resolve(response);
+                } else {
+                    // failure 
+                    reject(response);
+                }
+            }
+        };
+
+        // send request
+        httpClient.send(JSON.stringify(data));
+
+    });
+}
 
 
 //init Round of 16
@@ -64,13 +69,118 @@ for (i=0; i<_final.length;i++){
 
 
 
+// check if the user change his guess in lower round off the playoff
+function valid_stage(high,low,default_name){
+    for (var i=0; i<high.length;i++){
+        if (high[i].innerHTML != low[i*2].innerHTML &&
+                high[i].innerHTML != low[i*2 + 1].innerHTML){
+            high[i].innerHTML=default_name;
+        }
+    }
+
+}
+
+ 
 function chooseWinner(prevStage,currentStage,index){
     var j = Math.floor(index/2);
 
     if(prevStage[index].innerHTML != "QFinal" &&
     prevStage[index].innerHTML != "Semi-Final" &&
     prevStage[index].innerHTML != "Final"){
-
         currentStage[j].innerHTML=prevStage[index].innerHTML;
     }
+
+    valid_stage(s_final,q_final,"Semi-Final");
+    valid_stage(_final,s_final,"Final");
+    valid_stage(_winner,_final,"Winner!");
+}
+
+
+
+document.getElementById("submit").addEventListener("click",submitGuess); 
+
+
+let transform_data = arr => Array.from(arr).map(item => item.innerHTML);
+function validate_array(arr) {
+    for (let item of arr) {
+        let valid = _teams.some( val => val == item );
+        if (!valid) return false;
+    }
+    return true;
+}
+
+function validate_all_arrays() {
+    let arrays = [q_final, s_final, _final, _winner];
+    for (let arr of arrays) {
+        let transformed = transform_data(arr);
+        console.log(transformed);
+        if (!validate_array(transformed)) {
+            console.log("BAD ARRAY!");
+            return false;
+        }
+    }
+    console.log("array is ok.");
+    return true;
+}
+
+
+function submitGuess(){
+    var user = document.getElementById("add_user").value;
+
+    if (!validate_all_arrays()) {
+        console.log("DAMNIT!");
+        return;
+    }
+
+    let guess = {
+        q: transform_data(q_final),
+        s: transform_data(s_final),
+        fin: transform_data(_final),
+        win: transform_data(_winner),
+        user: user
+    };
+
+    console.log(guess);
+
+    //Fix it
+    request('POST', '/guess', guess).then(
+        message => {
+            _message.innerHTML=message;
+        },
+        message => {
+            _message.innerHTML=message;
+        }
+    );
+
+}
+
+
+
+
+
+window.onload = function(){
+
+    var modal = document.getElementById('myModal');
+    var span = document.getElementsByClassName("close")[0];
+    var guessBtn = document.getElementById("guessBTN");
+
+
+
+    // When the user clicks on the button, open the modal 
+    guessBtn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
 }
--- server.js
+++ server.js
@@ -17,17 +17,76 @@
 // //Saying that user made request twice because browser also makes a request for the favicon
 
 // grab the packages we need
+
+const q_final_results = ["Uruguay","France","Brazil","Belgium",
+"Russia","Croatia","Sweden","England"];
+const s_final_results = ["France","Belgium","Croatia","England"];
+const final_results = ["France","Croatia"];
+const the_winner = ["France"];
+
+
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
 // routes will go here
-
+app.use(express.json());
 app.use(express.static(__dirname + "/Project/"));
 
 // start the server
 app.get('/', function(req, res){
     res.sendfile('Html/Home.html', { root: __dirname + "/Project/" } );
 });
+
+
+function check(guess,res,points){
+    var sum = 0;
+    for(var i=0;i<guess.length;i++){
+        if (guess[i] == res[i]){
+            sum+=points;
+        }
+    }
+    return sum;
+}
+
+app.post('/guess', function(req, res) {
+    let data = req.body;
+    let q = data.q;
+    let s = data.s;
+    let fin = data.fin;
+    let win = data.win;
+    let score = 0;
+    
+    console.log(req.body);
+    //TODO!!!
+    
+    if (/*data.user in database*/ false) {
+        res.status(409).send("this username exists already!");
+        return;
+    }
+
+
+
+    // function to iterate over both arrays
+    score += check(q,q_final_results,1);
+    score += check(s,s_final_results,3);
+    score += check(fin,final_results,5);
+    score += check(win,the_winner,10);
+    console.log(score);
+
+    res.send("Your guess has been sent");
+
+    //TODO!!!
+    
+    /*insertToDB({user: data.user, score: score}).then(
+        success => res.send(score),
+        err => res.status(500).send("OOPS:(")
+    );
+    */
+});
+
+
+
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);
