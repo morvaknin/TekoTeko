6d516148210142c16b2fab6f56f9d958a39aaa4f
Mor Vaknin <morhai91@gmail.com>
2018-09-25 15:38:26 +0300

    working on database1

---
+++ db.js
@@ -0,0 +1,85 @@
+var mongodb = require('mongodb');
+var db;
+
+function connect() {
+    return new Promise( (resolve, reject) => {
+        var url = 'mongodb://localhost:27017/';
+        mongodb.MongoClient.connect(url, { useNewUrlParser: true }, (err, my_db) => {
+            if (err) {
+                reject(err);
+            }
+            db = my_db.db("TekoTeko");
+            db.createCollection("users", (err, res) => {
+                if (err){
+                    reject(err);
+                } 
+                resolve("Connection Established");
+            });
+        });
+    });
+}
+
+
+ function insert(user) {
+    return new Promise( (resolve, reject) => {
+        db.collection("users").insertOne(user, (err, res) => {
+            if (err){
+                reject(err);
+            } 
+            else 
+                resolve(res);
+        });
+    });
+}
+
+function find(user, pro) {
+    var proj = pro || {};
+    Object.assign(proj, { _id: 0 });
+
+    return new Promise((resolve, reject) => {
+        db.collection("users").findOne(user, { pro: proj }, (err, res) => {
+            if (err){
+                reject(err);
+            } 
+            resolve(res);
+        });
+    });
+}
+
+
+
+/*
+router.get('/_list',function(req,res){
+    var MongoClient = mongodb.MongoClient;
+    var url =  'mongodb://localhost:27017/scores';
+
+    MongoClient.connect(url,function(err,db){
+        if (err){
+            console.log("Unable to connect the server", err);
+        }
+        else{
+            console.log("Connection Established");
+            var collection = db.collection('scores');
+            collection.find({}).toArray(function(err,result){
+                if (err){
+                        res.send(err);
+                }
+                else if(result.length){
+                    res.render('scoreslist',{
+                        "scoreslist":resule
+                    });
+                }
+                else {
+                    res.send('No documants found');
+                }
+
+                db.close();
+            })
+        }
+
+    })
+})
+*/
+router.get('/newuser',function(req,res){
+    res.render('newuser',)
+})
--- server.js
+++ server.js
@@ -1,22 +1,16 @@
-// var http = require("http");
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 8080;
 
-// //This is the callback method, is called every time a user makes a request
-// //Request object has info about their request, response object is what we send back to them
-// function onRequest(request, response) {
-//     console.log("A user made a request" + request.url);
-//     response.writeHead(200, {"Content-Type": "text/plain"});
-//     response.write("I am a simple Node server!");
-//     response.end();
-// }
 
-// //Create a server and listen for requests on this port
-// http.createServer(onRequest).listen(8888);
-// console.log("Server is now running...");
+var mongo = require('./db').connect;
 
-// //Now open Chrome and go to http://localhost:8888
-// //Saying that user made request twice because browser also makes a request for the favicon
 
-// grab the packages we need
+// routes will go here
+app.use(express.json());
+app.use(express.static(__dirname + "/Project/"));
+
+
 
 const q_final_results = ["Uruguay","France","Brazil","Belgium",
 "Russia","Croatia","Sweden","England"];
@@ -25,14 +19,6 @@ const final_results = ["France","Croatia"];
 const the_winner = ["France"];
 
 
-
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-
-// routes will go here
-app.use(express.json());
-app.use(express.static(__dirname + "/Project/"));
 
 // start the server
 app.get('/', function(req, res){
